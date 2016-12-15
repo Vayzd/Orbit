fdee80c42fcc6f8e19f584f637e643ad6f979d3c
MrRumpunsh <maxschagginger@icloud.com>
2016-12-15 20:54:43 +0100

    using HikariCP for fast connection pooling

--- database/src/main/java/net/vayzd/orbit/database/Database.java
+++ database/src/main/java/net/vayzd/orbit/database/Database.java
@@ -1,4 +1,6 @@
 package net.vayzd.orbit.database;
 
 public interface Database {
+
+    void connect(String host, int port, String username, String password, String database);
 }
---
+++ database/src/main/java/net/vayzd/orbit/database/OrbitDatabase.java
@@ -0,0 +1,9 @@
+package net.vayzd.orbit.database;
+
+public class OrbitDatabase implements Database {
+
+    @Override
+    public void connect(String host, int port, String username, String password, String database) {
+
+    }
+}
