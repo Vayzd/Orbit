efe8232b4af1f638aa9d00fc25621d786329e531
MrRumpunsh <maxschagginger@icloud.com>
2016-12-24 15:08:24 +0100

    added MIT licence to some files and fixed some bugs

--- spigot/src/main/java/net/vayzd/orbit/spigot/OrbitPlugin.java
+++ spigot/src/main/java/net/vayzd/orbit/spigot/OrbitPlugin.java
@@ -1,3 +1,27 @@
+/*
+ * This file is part of Orbit, licenced under the MIT Licence (MIT)
+ *
+ * Copyright (c) Vayzd Network <https://www.vayzd.net/>
+ * Copyright (c) contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package net.vayzd.orbit.spigot;
 
 import net.vayzd.orbit.database.*;
@@ -23,8 +47,7 @@ public class OrbitPlugin extends JavaPlugin {
         saveDefaultConfig();
         getLogger().info("Connecting to database...");
         try {
-            OrbitAdapter adapter = new OrbitAdapter(getLogger(),
-                    Type.valueOf(getConfig().getString("database.type")),
+            OrbitAdapter adapter = new OrbitAdapter(Type.valueOf(getConfig().getString("database.type")),
                     getConfig().getString("database.host"),
                     getConfig().getInt("database.port"),
                     getConfig().getString("database.username"),
